7b37cf23a911a676bf0144ab40ebb15aafc29e0e
Ericmarcellini <delacresta@gmail.com>
2024-07-14 15:40:22 +0200

    creation of the card component, implementation in the app function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import Navbar from './components/Navbar.js';
+import Button from 'react-bootstrap/Button';
+import MyCard from './components/Card.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>Welcome to Fridge Poppa</h1>
-      {/* Other components or content */}
+      <h4>-Air conditioner Repair & Installation</h4>
+      <h1>We're Fridge Poppa LLC</h1>
+      <Button variant="primary">FREE ESTIMATES</Button>{' '}
+      <div className='container mt-4'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <MyCard />
+          </div>
+          <div className='col-md-4'>
+            <MyCard />
+          </div>
+          <div className='col-md-4'>
+            <MyCard />
+          </div>
+        </div>
+      </div>
+    { /*  content goes here -> */}
     </div>
   );
 }
